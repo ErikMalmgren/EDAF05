d1e2271ea6363e77e879a48e5ed6e4a2a7adf37a
Isak <isak@maatta.se>
2024-03-18 15:15:52 +0100

    pog

--- 1stablematching/lab1.py
+++ 1stablematching/lab1.py
@@ -2,19 +2,24 @@ import sys
 
 def main():
   students, companies = parse()
-  print(students)
-  print(companies)
+  unmatched_students = list(students)
+
+  while len(unmatched_students) != 0:
+    current_student = unmatched_students.pop()
+    most_prefered_company = students.get(current_student)[0]
+    
   
 def parse():
-  students = []
-  companies = []
+  students = dict()
+  companies = dict()
   N = int(next(sys.stdin))
   for idx, line in enumerate(sys.stdin):
-    tempLine = [int(x) for x in line.rstrip().split()]
+    line = line.rstrip().split(" ")
+    temp_line = [int(x) for x in line[1:]]
     if idx < N:
-        students.append(tempLine)
+      students[idx +1] = temp_line
     else:
-      companies.append(tempLine)
+      companies[idx + 1 - N] = temp_line
   return students, companies
 
 main()
