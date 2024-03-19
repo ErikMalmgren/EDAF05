0c27a06a8b3c78c73aea8ea88faf4cc1c97e3eb5
Erik Malmgren <er8540ma-s@student.lu.se>
2024-03-19 14:42:19 +0100

    till small messy

--- 1stablematching/lab1.py
+++ 1stablematching/lab1.py
@@ -1,37 +1,64 @@
 import sys
 
 def main():
-  gs()
+  gs2()
   
-
+  
 def gs():
   students, companies = parse2()
   unmatched_students = list(students)
   matches = {}
+  
+  while unmatched_students:
+    current_student = unmatched_students.pop(0)
+    student_companies = students.get(current_student)
+    current_index = 0
+    current_company = student_companies[current_index]
+    
+    if current_company not in matches:
+      matches[current_company] = current_student
+    elif companies[current_company].index(current_student) > companies[current_company].index(matches[current_company]):
+      old_student = matches[current_company]
+      matches[current_company] = current_student
+      unmatched_students.append(old_student)
+    else:
+      unmatched_students.append(current_student)
+    current_index += 1
+
+def gs2():
+  students, companies = parse2()
+
+  print(students)
+  print(companies)
+  unmatched_students = list(students)
+  matches = {}
 
   while unmatched_students:
-    current_student = unmatched_students.pop()
+    # print("unmatched students: ", unmatched_students)
+    # print("matches: ", matches)
+    current_student = unmatched_students.pop(0)
     student_companies = students.get(current_student)
-    print("student: ", current_student)
-    print("stud comps: ", student_companies)
 
     for comp in student_companies:
-      if current_student not in matches.values():
+      if comp not in matches:
         matches[comp] = current_student
         break
 
       elif companies[comp].index(matches[comp]) < companies[comp].index(current_student):
+        # print("comp ", matches[comp])
         unmatched_students.append(matches[comp])
         matches[comp] = current_student
+        
+        break
     
-    if current_student not in matches: 
+    if current_student not in matches.values():
       unmatched_students.append(current_student)
-    print("matches ", matches) 
+    # print("slut", matches)
 
   printMatches(matches)   
 
 def printMatches(matches):
-  print(matches)
+  # print(matches)
   for i in range(len(matches)):
     print(matches[i +1])
 
