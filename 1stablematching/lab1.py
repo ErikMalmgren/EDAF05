0d26ed9f4bbdadab67ede234e4a24fba999bce33
Isak <isak@maatta.se>
2024-03-21 12:20:02 +0100

    donezo lab1

--- 1stablematching/lab1.py
+++ 1stablematching/lab1.py
@@ -2,52 +2,31 @@ import sys
 import time
 
 def main():
-  t1 = time.time()
   gs()
-  print(time.time() - t1)
 
 
 def gs():
   students, companies = parse()
-
-
-  #print("Students", students)
-  # print(companies)
   unmatched_students = list(students)
   matches = {}
   while unmatched_students:
-    # print("unmatched students: ", unmatched_students)
-    # print("matches: ", matches)
     current_student = unmatched_students.pop(0)
-    # print("current student: ", current_student)
     student_companies = students.get(current_student)
-    # print("student's companies: ", student_companies)
 
     for comp in student_companies:
-      #print("Company list:", comp, " ", companies[comp])
-      #print(comp not in matches)
       if comp not in matches:
         matches[comp] = current_student
         break
 
-      #print("nuvarande match:" , companies[comp].index(matches[comp]))
-      #print("potentiell match: ", companies[comp].index(current_student))
       if companies[comp].index(matches[comp]) > companies[comp].index(current_student):
-        # print("comp ", matches[comp])
         unmatched_students.append(matches[comp])
         matches[comp] = current_student
-        
         break
-    
-    if current_student not in matches.values():
-      unmatched_students.append(current_student)
-    #print("slut", matches)
-    #print("----------------")
+
 
   printMatches(matches)   
 
 def printMatches(matches):
-  #print(matches)
   for i in range(len(matches)):
     print(matches[i +1])
 
