aa0e5711f801a521c85f636e85611401b254714d
Isak <isak@maatta.se>
2024-04-15 14:59:37 +0200

    sub 40

--- 3makingfriends/lab3.py
+++ 3makingfriends/lab3.py
@@ -1,27 +1,23 @@
 import sys
 from heapq import heappop, heappush
 
+
 def main():
-  graph = parse()
-  mst = prim(graph)
-  res = sum([v for k, v in mst.values()])
-  print(res)
+  print(sum([v for k, v in prim(parse()).values()]))
 
 def parse():
-  inp = sys.stdin.read().split('\n')
-  num_people, num_pairs = inp.pop(0).split(" ")
-  inp.pop(-1)
+  inp = sys.stdin.read().strip().split('\n')
   graph = {}
-  for line in inp:
-    u, v, w = map(int, line.split()) # u, v are indices of the persons of the edge and w is the weight of the edge
+  for line in inp[1:]:
+    u, v, w = map(int, line.split())
     graph.setdefault(u, []).append((v, w))
     graph.setdefault(v, []).append((u, w))
   return graph
 
+
 def prim(graph):
   tree = {}
   visited = set()
-  
   root = next(iter(graph))
   visited.add(root)
   queue = [(neighbor, weight) for weight, neighbor in graph[root]]
@@ -37,8 +33,6 @@ def prim(graph):
       for n, w in graph[neighbor]:
         if n not in visited:
           heappush(queue, (w, n))
-  
   return tree
-  
-  
+
 main()
