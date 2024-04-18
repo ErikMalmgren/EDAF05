7c3b53594ddcf8ab224708b465ee4249596a2c0a
Isak <isak@maatta.se>
2024-04-18 14:57:48 +0200

    graham cracker fungerar tror vi

--- 4closestpair/lab4.py
+++ 4closestpair/lab4.py
@@ -28,10 +28,30 @@ def graham_scan(n, points):
     h.append(point_t)
   for point in points:
     point += t
-  n = len(h)
-  for point in h:
-    print(point)
+  return h
     
+def k(p, q):
+  return (q.y - p.y) / (q.x - p.x)
+
+def compute_k(points):
+  n = len(points)
+  alpha = []
+  for i in range(n):
+    alpha.append(k(points[i], points[i + 1 % n])) 
+  return alpha
+
+def add(k, fromage, to, q, p, n):
+  j = fromage
+  q[k] = p[j]
+  k += 1
+  i = j
+  j = (j + 1) % n
+  while i != to:
+    q[k] = p[j]
+    k += 1
+    i = j
+    j = (j + 1) % n
+  return k
 
 def direction(p1, p2, p3):
   return (((p2.x - p1.x) * (p3.y - p1.y)) - ((p2.y - p1.y) * (p3.x - p1.x))) < 0
