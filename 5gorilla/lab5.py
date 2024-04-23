bf1fc262a2cfe6c2aa53f9e0ff69089914fcaf16
Isak <isak@maatta.se>
2024-04-23 16:20:31 +0200

    fuck score matrix

--- 5gorilla/lab5.py
+++ 5gorilla/lab5.py
@@ -2,10 +2,11 @@ import sys
 
 def main():
   matrix, queries = parse()
-  gap_score = 4
+  gap_score = -4
   memo = {}
   for q in queries:
-    needleman_wunsch(matrix, q[0], q[1], -4, memo)
+    res = needleman_wunsch(matrix, q[0], q[1], gap_score, memo)
+    print(res[0] + " " + res[1])
   
 def parse():
   inp = sys.stdin.read().strip().split('\n')
@@ -19,22 +20,13 @@ def parse():
     queries.append(q.split())
   return matrix, queries
 
-def match_score(c1, c2, gap_score):
+def match_score(c1, c2, gap_score, cost):
   if c1 == c2:
-    return 1
-  else:
+    return cost
+  elif c1 == '*' or c2 == '*':
     return gap_score
-
-def print_matrix(mat):
-    for i in range(0, len(mat)):
-        print("[", end = "")
-        for j in range(0, len(mat[i])):
-            print(mat[i][j], end = "")
-            if j != len(mat[i]) - 1:
-                print("\t", end = "")
-        print("]\n")
-
-
+  else:
+    return -1
   
 def create_score_matrix(matrix, string1, string2, gap_score):
   score_matrix = []
@@ -50,9 +42,15 @@ def create_score_matrix(matrix, string1, string2, gap_score):
   for j in range(len(string1) + 1):
     score_matrix[0][j] = gap_score * j
     
+  print(string1, string2)
+    
   for i in range(1, len(string2) + 1):
     for j in range(1, len(string1) + 1):
-      match = score_matrix[i - 1][j - 1] + match_score(string1[j - 1], string2[i - 1], gap_score)
+      print("i: ", i, " j: ", j, '\n')
+      match = score_matrix[i - 1][j - 1] + match_score(string1[j - 1], string2[i - 1], gap_score, matrix[i][j])
+      print("match_score parametrar: ", string1[j - 1], string2[i - 1], matrix[j-2][i-2])
+      print("match_score värde: ", match_score(string1[j - 1], string2[i - 1], gap_score, matrix[i][j]), '\n')
+      
       insert = score_matrix[i][j - 1] + gap_score
       delete = score_matrix[i - 1][j] + gap_score
       score_matrix[i][j] = max(match, insert, delete)
@@ -73,6 +71,35 @@ def needleman_wunsch(matrix, string1, string2, gap_score, memo):
     score_diagonal = score[i-1][j-1]
     score_up = score[i][j-1]       
     score_left = score[i-1][j]
+    
+    if score_current == score_diagonal + match_score(string1[j-1], string2[i-1], gap_score, matrix[j - 2][i - 2]):
+      res1 += string1[j-1]
+      res2 += string2[i-1]
+      i -= 1
+      j -= 1
+    elif score_current == score_up + gap_score:
+      res1 += string1[j-1]
+      res2 += '*'
+      j -= 1
+    elif score_current == score_left + gap_score:
+      res1 += '*'
+      res2 += string2[i-1]
+      i -= 1
+
+  # Finish tracing up to the top left cell
+  while j > 0:
+    res1 += string1[j-1]
+    res2 += '*'
+    j -= 1
+  while i > 0:
+    res1 += '*'
+    res2 += string2[i-1]
+    i -= 1
+    
+  res1 = res1[::-1]
+  res2 = res2[::-1]
+    
+  return(res1, res2)
 
   
 main()
