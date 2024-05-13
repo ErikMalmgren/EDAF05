1436291f48eb565bd6eec713f71f13196c147c3e
Isak <isak@maatta.se>
2024-05-13 19:49:45 +0200

    lab 6 deque op

--- 6railwayplanning/lab6new.py
+++ 6railwayplanning/lab6new.py
@@ -1,27 +1,25 @@
 import sys
 from collections import deque
 
+
 def main():
   capacity, routes, routes_to_remove, min_capacity = parse()
   findX(capacity, routes, routes_to_remove, min_capacity)
-  
+
 
 def findX(capacity, routes, routes_to_remove, min_capacity):
-  
   start = 0
   end = len(routes_to_remove)
-  mid = (start + end) // 2 
+  mid = (start + end) // 2
   remove_counter = 0
-  while(start <= end or current_capacity < min_capacity):
-    mid = (start + end) // 2 
+  while (start <= end or current_capacity < min_capacity):
     capacity_copy = [row[:] for row in capacity]
+    mid = (start + end) // 2
     for i in range(mid):
       u, v = routes[routes_to_remove[i]]
       capacity_copy[u][v] = 0
       capacity_copy[v][u] = 0
-      
     current_capacity = preflow_push(capacity_copy)
-    
     if current_capacity >= min_capacity:
       start = mid + 1
     else:
@@ -29,7 +27,7 @@ def findX(capacity, routes, routes_to_remove, min_capacity):
       end = mid - 1
   res = str(remove_counter - 1) + ' ' + str(current_capacity)
   print(res)
-  
+
 
 def parse():
   inp = sys.stdin.read().strip().split('\n')
@@ -47,24 +45,24 @@ def parse():
     routes_to_remove.append(int(inp[i]))
   return capacity, routes, routes_to_remove, nbr_of_students
 
+
 def preflow_push(capacity):
   n = len(capacity)
   height = [0] * n
   ef = [0] * n
   seen = [0] * n
-  
   F = [[0] * n for _ in range(n)]
-  
+
   def push(u, v):
     delta = min(ef[u], capacity[u][v] - F[u][v])
     F[u][v] += delta
     F[v][u] -= delta
     ef[u] -= delta
     ef[v] += delta
-    
+
   def relabel(u):
     height[u] += 1
-    
+
   def visit_neighbors(u):
     while ef[u] > 0:
       if capacity[u][seen[u]] - F[u][seen[u]] > 0 and height[u] > height[seen[u]]:
@@ -76,22 +74,19 @@ def preflow_push(capacity):
         seen[u] = 0
         nodes.append(u)
 
-  
   height[0] = n
   ef[0] = float('inf')
   for v in range(n):
     push(0, v)
-    
-  p = 0
+
   nodes = deque(range(1, n - 1))
-  
   while nodes:
     u = nodes.popleft()
     old_height = height[u]
     visit_neighbors(u)
     if height[u] > old_height:
       nodes.appendleft(u)
+  return sum(F[0])
 
-  return sum(F[0])        
 
 main()
